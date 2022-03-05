0e314ec25e308b1052cc965d8ee0d81f628dd887
Mario Josué Tavarez Mendoza <mario@MacBook-Pro-de-Mario.local>
2022-03-05 16:17:48 -0600

    VITE PLUGIN COMPRESS: Has been added vite plugin compress with algorithm brotli

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           </button>
         </p>
         <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
+          Editar rweigur <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
           <a
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+// Plugins
+import host from 'vite-plugin-host';
+import viteCompression from 'vite-plugin-compression';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    host(),
+    viteCompression({
+      algorithm: "brotliCompress",
+      verbose: true,
+    }),
+  ],
+});
